74e18cc5cb34703fd0665ff6aa50061f4598dcbc
Harini <harinipradeep27@gmail.com>
2016-12-29 17:57:52 +0530

    With custom roles restrict cinder operations.
    1.Get the project and user name as input from user.
    2.Checks if the project already exists,else create a new project.
    3.Checks if the user exists, if not create a new user.
    4.For new created user, assign the role with access to all cinder
    operations or with only delete operation access or with all cinder
    operation except delete access.
    5.If the user is having delete access, add him to a custom group.
    6.Authorize the user and project details using keystone.
    7.Create, List or Delete volumes as per the requirement.

--- openstack/workflow/ksworkflow.py
+++ openstack/workflow/ksworkflow.py
@@ -6,15 +6,27 @@ from cinderclient import client as ciclient
 
 #get input from user
 prj=str(raw_input("Enter Project name :\n"))
-grp=str(raw_input("Enter Group name: \n"))
+#rp=str(raw_input("Enter Group name: \n"))
 usr=str(raw_input("Enter User name: \n"))
-voltp=str(raw_input("Enter Type of the volume required : \n"))
-volnm=str(raw_input("Enter Name of the volume required : \n"))
-sz=int(raw_input("Enter size of the volume: \n"))
 
-print prj,grp,usr,voltp,sz
+#Depending upon the access assign role to user and add to the group
+def access_role(user,project,access):
+ ru = keystone.roles.find(name="user")
+ rd = keystone.roles.find(name="vol_del")
+ gru = keystone.groups.find(name="cinder_delete")
+ if access == 1:
+  keystone.roles.grant(ru,user=user,project=project)
+  keystone.roles.grant(rd,user=user,project=project)
+  keystone.users.add_to_group(user, gru)
+ elif access == 2:
+  keystone.roles.grant(ru,user=user,project=project)
+ else:
+  keystone.roles.grant(rd,user=user,project=project)
+  keystone.users.add_to_group(user, gru)
+   #rg = keystone.roles.grant(ru, group=grouptouse, project=projtouse)
+   #print "User created"
 
-#validate project,group and user info using keystone
+#validate project and user info using keystone
 auth = v3.Password(user_domain_name='default', username='admin',password='Nexii123',project_domain_name='default', project_name='admin', auth_url='http://localhost:5000/v3')
 sess = session.Session(auth=auth)
 keystone = client.Client(session=sess)
@@ -26,61 +38,71 @@ try:
  pf= keystone.projects.find(name=prj)
  if pf:
   projtouse = pf
-  print "Project "+str(projtouse.to_dict()['name'])+" exists"
+  #print "Project "+str(projtouse.to_dict()['name'])+" already exists.Using the same"
  else:
   projtouse = keystone.projects.create(prj, d[0])
-  print "Project created"
+  print "New Project created"
 except:
  projtouse = keystone.projects.create(prj, d[0])
- print "project created"
+ print "New project created"
 
-#check for group
-try:
- gf = keystone.groups.find(name=grp)
- if gf:
-  grouptouse=gf
-  print "Group exists"
-  grouptouse=gf
-  print "Group exists"
- else:
-  pass
-except:
- grouptouse = keystone.groups.create(name=grp, domain=d[0])
 
 #check for users
 #uf = keystone.users.list()
-ru = keystone.roles.find(name="user")
 try:
  uf = keystone.users.find(name=usr, project=projtouse)
  if uf:
   usertouse = uf
-  print "User "+str(usertouse.to_dict()['name'])+" exists"
+  #print "User "+str(usertouse.to_dict()['name'])+" exists"
 except:
- try:
-  uv = keystone.groups.list(user=uf)
-  if uv:
-   usertouse=uv
-  else:
-   pass
- except:
-   pas=str(raw_input("Enter password for user: "+usr+"\n"))
-   usertouse  = keystone.users.create(name=usr, password=pas, project=projtouse, domain=d[0])
-   uag = keystone.users.add_to_group(usertouse, grouptouse)
-   rg = keystone.roles.grant(ru, group=grouptouse, project=projtouse)
-   print "User created"
+ #try:
+ #uv = keystone.groups.list(user=uf)
+ #if uv:
+  #usertouse=uv
+  #else:
+  # pass
+ #except:
+ #pas=str(raw_input("Enter password for user: "+usr+"\n"))
+ pas=usr
+ usertouse  = keystone.users.create(name=usr, password=pas, project=projtouse, domain=d[0])
+ access=int(raw_input("User has been newly created. Please select kind of access required\n 1.All volume operations\n 2.All volume operations except volume delete \n 3.Only volume delete\n"))
+ access_role(usertouse,projtouse,access)
+
 
 #Use the values given by user after validation
 projectname=projtouse.to_dict()['name']
 username=usertouse.to_dict()['name']
 password=str(raw_input("Enter password for user "+str(usertouse.to_dict()['name'])+": "))
-print projectname,username,str(password)
+#print projectname,username,str(password)
 
 #Logging in using the given values
 auth1 = v3.Password(user_domain_name='default', username=username,password=password,project_domain_name='default', project_name=projectname, auth_url='http://controller:5000/v3')
 sess1 = session.Session(auth=auth1)
 cin = ciclient.Client('2.0',session=sess1)
-#Creating volumes
-myvol=cin.volumes.create(name=volnm,size=sz,volume_type=voltp)
-print "Volume created successfully with volume id: "+myvol.id
-                                                                        
+
+val = int(raw_input("Select any cinder operation to perform\n 1.Create \n2.List \n3.Delete \n"))
+if val==1:
+ voltp=str(raw_input("Enter Type of the volume required(gold or silver) : \n"))
+ volnm=username+projectname+voltp
+ sz=int(raw_input("Enter size of the volume: \n"))
+ #Creating volumes
+ myvol=cin.volumes.create(name=volnm,size=sz,volume_type=voltp)
+ print "Volume created successfully with volume id: \n"+myvol.id
+elif val==2:
+ #print cin.volumes.list()
+ if not cin.volumes.list():
+  print "No volumes are present in this project"
+ else:
+  for i in cin.volumes.list():
+   print i
+   print "size : ",i.to_dict()['size']
+   print "name : ",i.to_dict()['name']
+   print "status : ",i.to_dict()['status']
+   print "volume_type : ", i.to_dict()['volume_type']
+   print "\n"
+else:
+ vol_name = str(raw_input("Enter the volume name to be deleted: \n"))
+ vn = cin.volumes.find(name=vol_name)
+ vid = vn.to_dict()['id']
+ cin.volumes.delete(vid)
 
